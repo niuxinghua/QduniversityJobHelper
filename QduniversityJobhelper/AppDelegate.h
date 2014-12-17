6e6273c32c2efec6c90b8c4e6c7f345fad49aa44
niuxinghua <970626879@qq.com>
2014-12-17 15:18:15 +0800

    Initial Commit

---
+++ QduniversityJobhelper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QduniversityJobhelper
+//
+//  Created by niuxinghua on 14-12-17.
+//  Copyright (c) 2014年 niuxinghua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
